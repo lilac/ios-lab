958a0c7e0f91a83cd5db428fa5a823f9d07350a0
Junjun Deng <cnjamesdeng@gmail.com>
2016-09-20 21:46:54 +1000

    Initial Commit

---
+++ ios-lab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-lab
+//
+//  Created by Junjun Deng on 20/09/2016.
+//  Copyright © 2016 Junjun Deng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
